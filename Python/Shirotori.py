8b74614c00dd7455a0f9a14b9ba74d616e5d3e74
cob0013 <48230744+cob0013@users.noreply.github.com>
2019-09-13 16:05:36 -0500

    updated shirotori

--- Python/Shirotori.py
+++ Python/Shirotori.py
@@ -1,4 +1,22 @@
 n = int(input())
-words = {}
-for i in n:
-	words.add(i)
+previous = input()
+count = 1
+words = set()
+words.add(previous)
+for i in range(n):
+	word = input()
+	if  (word not in words) and (previous[len(previous) - 1] == word[0]):
+		words.add(word)
+		previous = word
+		count += 1
+		if (count == n):
+			print ("Fair Game")
+			break
+	else:
+		if i % 2 == 0:
+			print("Player 2 lost")
+			break
+		else:
+			print("Player 1 lost")
+			break
+
