b124db4b4618347b18625c2d08c63f19a41550b0
cob0013 <cob0013@auburn.edu>
2020-10-15 23:54:13 -0500

    pls

--- Python/Ladder.py
+++
@@ -1,5 +0,0 @@
-import math	
-line = list(map(float, input().split()))
-h = line[0]
-ang = line[1]
-print(math.ceil((h  / (math.sin(math.radians(ang))))))
--- Python/Spavanac.py
+++
@@ -1,9 +0,0 @@
-h,m = input().split()
-h = int(h)
-m = int(m)
-if (m > 45 and m != 0):
-	print(str(h) + " " + str((m-45)))
-elif (h == 0):
-	print(str(23) + " " + str(m + 15))
-else:
-	print(str(h -1) + " " + str(m +15))
